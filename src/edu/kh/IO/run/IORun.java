5cfca023afdebc6c7dac5c29c8cf02d79029bfef
leewoo-jin <fkaustkwnfrk@gmail.com>
2023-08-02 15:30:01 +0900

    20230802

---
+++ src/edu/kh/IO/run/IORun.java
@@ -0,0 +1,19 @@
+package edu.kh.IO.run;
+
+import edu.kh.IO.model.service.IOService;
+
+public class IORun {
+
+	public static void main(String[] args) {
+		
+		IOService service = new IOService();
+
+		//service.output1();
+		//service.output2();
+		//service.input1();
+		service.input2();
+		
+		
+	}
+
+}
